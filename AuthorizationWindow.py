1883adfeb680852943588749c59ad467f32e4a9c
klyushkin <stepan.9h88@mail.ru>
2024-05-20 16:33:44 +0300

    Create AuthorizationWindow, DBConnection

---
+++ AuthorizationWindow.py
@@ -0,0 +1,53 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QRadioButton, QPushButton, QLabel, QMainWindow
+from ConnectionManager import ConnectionManager
+from MainWindow import MainWindow
+
+class AuthorizationWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        super().__init__()
+        self.setWindowTitle('Выбор роли')
+        self.setGeometry(100, 100, 300, 150)
+
+        layout = QVBoxLayout()
+
+        self.label = QLabel('Выберите роль:', self).setFixedWidth(200)  
+        
+
+        self.user_radio = QRadioButton('Пользователь', self)
+        self.admin_radio = QRadioButton('Администратор', self)
+
+        self.select_button = QPushButton('Выбрать', self)
+        self.select_button.clicked.connect(self.onSelectUserButtonClicked)
+
+        layout.addWidget(self.user_radio)
+        layout.addWidget(self.admin_radio)
+        layout.addWidget(self.select_button)
+
+        self.setLayout(layout)
+
+    def onSelectUserButtonClicked(self):
+        global IS_ADMIN
+        if self.user_radio.isChecked():
+            connection = ConnectionManager("hotelDatabaseAdmin")     
+            IS_ADMIN = True
+            self.form1 = MainWindow(connection, IS_ADMIN)
+            self.form1.show()
+            self.close()
+
+        elif self.admin_radio.isChecked():
+            connection = ConnectionManager("hotelDatabaseUser")
+            IS_ADMIN = False
+            self.form1 = MainWindow(connection, IS_ADMIN)
+            self.form1.show()
+            self.close()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    form = AuthorizationWindow()
+    form.show()
+    sys.exit(app.exec_())
---
+++ ConnectionManager.py
@@ -0,0 +1,26 @@
+import psycopg2
+
+
+class ConnectionManager:
+    def __init__(self, user):
+        self.__dbname = "hotelDatabase"
+        self.user = user
+        self.__password = "12345678"
+        self.__host = "localhost"
+
+    def __enter__(self):
+        try:
+            self.connection = psycopg2.connect(
+                dbname=self.__dbname,
+                user=self.user,
+                password=self.__password,
+                host=self.__host,
+                client_encoding="utf8",
+            )
+            print("Урррааа")
+            return self.connection
+        except psycopg2.Error as e:
+            print(f'Ошибка подключения к базе данных: {e}')
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.connection.close()
